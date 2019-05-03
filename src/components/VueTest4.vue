705476835b89551470d8d63953a9e297918fce1a
Will Patterson <wpatter6@gmail.com>
2019-05-03 17:31:10 -0600

    Add vue test 4

---
+++ src/components/VueTest4.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>This is vue test #4</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({});
+</script>
+
+<style lang="scss" scoped>
+</style>
